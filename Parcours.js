87583b787aaf65903331fa6dc8dfee12b9ee559d
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-08-06 00:18:54 +0200

    good commit

--- Parcours.js
+++ Parcours.js
@@ -1,72 +1,68 @@
-var personnes =
-  { 
-  "geeks" : {
-          "programmeurs": [
-              { "nom": "McLaughlin", "prenom": "Brett", "email": "brett@email.com" },
-              { "nom": "Hunter", "prenom": "Jason", "email": "jason@servlets.com" },
-              { "nom": "Harold", "prenom": "Elliot", "email": "elharo@macfaq.com" }
-   ],
-         "gamers" : [
-              { "nom": "Play1", "prenom": "Brett", "email": "brett@email.com" },
-              { "nom": "Play23", "prenom": "Paul", "email": "paull@servlets.com" },
-              { "nom": "Play33", "prenom": "Pierre", "email": "pierre@macfaq.com" }     
-          ]
-  },
-    
-  "auteurs": [
-      { "nom": "Asimov", "prenom": "Isaac", "genre": "science fiction" },
-      { "nom": "Williams", "prenom": "Tad", "genre": "horreur" },
-      { "nom": "Peretti", "prenom": "Frank", "genre": "policier" }
-    ],
-  "musiciens": [
-      { "nom": "Clapton", "prenom": "Eric", "instrument": "guitare" },
-      { "nom": "Rachmaninoff", "prenom": "Laurent", "instrument": "piano" }
-   ]
-  };
 
+(function(root, factory) {
 
-Parcours = {};
-Parcours.isBrowsable = function(obj) {
-   return (toString.call(obj) == '[object Array]' || Object(obj) === obj);    
- 
-}
-Parcours.find = function(object, items, index, iterator) {
-    if (index == items.length) {
-        return false;   
+    // if AMD loaded
+   if(typeof define == 'function' && define.amd) {
+       define(factory);
+   }
+   else {
+
+        var hasModule = (typeof module !== 'undefined' && module.exports); // true is we are in nodeJS env
+
+        if (hasModule) { 
+            module.exports = factory;
+        }
+        else { // browser env
+            root.Parcours = factory;
+        }
+
+   }
+
+})(this, function() {
+
+    // true is obj is Object or Array
+    var isBrowsable = function(obj) {
+        return (toString.call(obj) == '[object Array]' || Object(obj) === obj);
     }
-    if (typeof object[items[index]] != "undefined") {
+
+    var find = function(object, items, index, iterator) {
+         if (index == items.length) {
+            return false;   
+         }
+        if (typeof object[items[index]] != "undefined") {
        
         
-        if (typeof items[index+1] != "undefined") {
-            Parcours.find(object[items[index]], items, index+1, iterator)
-        }
-        else {
-            if (Parcours.isBrowsable(object[items[index]])) {
-                for(var i in object[items[index]]) {
-                    iterator(i, object[items[index]][i]);
-                }
+            if (typeof items[index+1] != "undefined") {
+                find(object[items[index]], items, index+1, iterator)
             }
             else {
-                 iterator(0, object[items[index]]);   
+                if (isBrowsable(object[items[index]])) {
+                    for(var i in object[items[index]]) {
+                        iterator(i, object[items[index]][i]);
+                    }
+                }
+                else {
+                    iterator(0, object[items[index]]);   
+                }
+            }
+        }
+        else {
+            for(var i in object) {
+                if(isBrowsable(object[i])) {
+                    find(object[i], items, index, iterator);   
+                } 
             }
         }
     }
-    else {
-        for(var i in object) {
-            if( Parcours.isBrowsable(object[i])) {
-               Parcours.find(object[i], items, index, iterator);   
-             }
-             
+
+    return {
+        forEach : function(object,item, iterator) {
+            var items = item.split('.');
+
+            find(object, items, 0, iterator);
         }
     }
-}
 
-Parcours.forEach = function(object, item, iterator) {
-     var items = item.split('.');
-    
-    Parcours.find(object, items, 0, iterator);
-}
 
-Parcours.forEach(personnes, "gamers.nom", function(key,val) {
-    console.log(key, val);
 });
+
